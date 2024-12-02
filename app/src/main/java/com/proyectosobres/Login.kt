3adcbf56e36981b33098d5a45a03bbef892b6838
zGarryxx <msevillagarrido@safareyes.es>
2024-12-02 22:44:20 +0100

    He creado el diseño correcto para Pixel 8 Pro en login, sign up y menu principal. Por favor no seais cabrones y usad Pixel 8 Pro como emulador

--- app/src/main/java/com/proyectosobres/Login.kt
+++ app/src/main/java/com/proyectosobres/Login.kt
@@ -19,12 +19,6 @@ class Login : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.login_estilo)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
         val mail = findViewById<EditText>(R.id.Email)
         val password = findViewById<EditText>(R.id.Contraseña)
         val botonAcceder = findViewById<Button>(R.id.btnAcceder)
