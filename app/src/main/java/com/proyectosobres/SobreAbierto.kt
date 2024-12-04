f986638dcbfd7a7d3a1dbf6bdfb8a6c3e6b9a695
jcanterla <jcanterlarufino@safareyes.es>
2024-12-04 13:01:25 +0100

    generacion aleatoria cartas desde base de datos

--- app/src/main/java/com/proyectosobres/SobreAbierto.kt
+++ app/src/main/java/com/proyectosobres/SobreAbierto.kt
@@ -2,11 +2,15 @@ package com.proyectosobres
 
 import DBcontrol
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 
 class SobreAbierto : Activity() {
+    private var invisibleCount = 0
+    private var nextImageIndex = 4
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sobreabierto_estilo)
@@ -25,16 +29,24 @@ class SobreAbierto : Activity() {
 
         // Mostrar las imágenes en los ImageViews
         for (i in images.indices) {
+            val imagePath = "file:///android_asset/${images[i]}"
             Glide.with(this)
-                .load(images[i])
+                .load(imagePath)
                 .placeholder(R.drawable.carta_menup)
                 .error(R.drawable.carta_menup)
                 .into(imageViews[i])
 
             imageViews[i].setOnClickListener {
-                it.visibility = ImageView.INVISIBLE // Hacer invisible la imagen cuando se toque
+                if (i == nextImageIndex) {
+                    it.visibility = ImageView.INVISIBLE // Hacer invisible la imagen cuando se toque
+                    invisibleCount++
+                    nextImageIndex--
+                    if (invisibleCount == 5) {
+                        val intent = Intent(this, MenuPrincipal::class.java)
+                        startActivity(intent)
+                    }
+                }
             }
         }
     }
 }
-
